c9c66b0eb945cf807eae57ee9f7d4a58643d186d
mohammed asfour <masfour@mun.ca>
2021-05-20 14:27:10 +0200

    solution to exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+# initial height and number of bounces
+height = 100
+bounces = 10
+
+for bounce in range(bounces):
+    height *= 3/5           # update the height
+    print(bounce+1, height) # bounce starts from 0
