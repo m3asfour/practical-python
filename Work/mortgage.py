9cd2b7e19e3f3a5f6715c085f3106e10866b0900
mohammed asfour <masfour@mun.ca>
2021-05-20 16:32:23 +0200

    solution to exercise 1.8

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,9 +6,12 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+months = 0
 
 while principal > 0:
-    principal = principal * (1 + rate/12) - payment
-    total_paid += payment
+    extra_payment = 1000 if months < 12 else 0
+    principal = principal * (1 + rate/12) - (payment + extra_payment)
+    total_paid += payment + extra_payment
+    months += 1
 
-print(f'Total Paid: {total_paid:0.2f}')
+print(f'Total Paid: {total_paid:0.2f}. It took {months} months.')
