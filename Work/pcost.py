33686c8bec60b74d08f411d7a221bd1d1410706c
mohammed asfour <masfour@mun.ca>
2021-05-23 23:28:37 +0200

    solution to exercise 1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,16 +2,20 @@
 #
 # Exercise 1.27
 
-line_num = 0
-all_shares_price = 0
+def portfolio_cost(filename):
+    line_num = 0
+    all_shares_price = 0
 
-with open('Data/portfolio.csv', 'rt') as f:
-    for line in f:
-        line_num += 1
-        if line_num == 1:
-            continue
+    with open(filename, 'rt') as f:
+        for line in f:
+            line_num += 1
+            if line_num == 1:
+                continue
 
-        stock_name, shares_num, stock_price = line.strip().split(',')
-        all_shares_price += int(shares_num) * float(stock_price)
+            stock_name, shares_num, stock_price = line.strip().split(',')
+            all_shares_price += int(shares_num) * float(stock_price)
+    
+    return all_shares_price
 
-print(f'Total cost {all_shares_price}')
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost:', cost)
