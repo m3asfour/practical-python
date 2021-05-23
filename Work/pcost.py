8884b82957083e2bb45bafaca830b58324875ece
mohammed asfour <masfour@mun.ca>
2021-05-23 23:24:15 +0200

    solution to exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
+
+line_num = 0
+all_shares_price = 0
+
+with open('Data/portfolio.csv', 'rt') as f:
+    for line in f:
+        line_num += 1
+        if line_num == 1:
+            continue
+
+        stock_name, shares_num, stock_price = line.strip().split(',')
+        all_shares_price += int(shares_num) * float(stock_price)
+
+print(f'Total cost {all_shares_price}')
