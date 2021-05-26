083a8ee4164b266c43708dd7cf06640589f0e927
mohammed asfour <masfour@mun.ca>
2021-05-26 12:45:47 +0200

    solution to exercise 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,27 @@
 # report.py
 #
 # Exercise 2.4
+
+
+import csv
+
+def read_portfolio(filename):
+    """parses a csv file into a list of tuples
+
+    Args:
+        filename (str): the path of the csv file
+
+    Returns:
+        list: list of tuples. each tuple contains info of one line
+    """
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)        # parse the csv file
+        headers = next(rows)        # skip the header line
+        for row in rows:            # loop over lines
+            # cast line contents into proper datatypes
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+
+    return portfolio    
