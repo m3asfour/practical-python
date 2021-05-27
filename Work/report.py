745daa78857121de4603383b393dfe6c1f5531b8
mohammed asfour <masfour@mun.ca>
2021-05-27 07:04:11 +0200

    solution to exercise 2.7

--- Work/report.py
+++ Work/report.py
@@ -1,6 +1,6 @@
 # report.py
 #
-# Exercise 2.6
+# Exercise 2.7
 
 
 import csv
@@ -22,9 +22,9 @@ def read_portfolio(filename):
         for row in rows:            # loop over lines
             # cast line contents into proper datatypes
             holding = {
-                'name': row[0], 
+                'name'  : row[0], 
                 'shares': int(row[1]),
-                'price': float(row[2])}
+                'price' : float(row[2])}
 
             portfolio.append(holding)
 
@@ -49,3 +49,19 @@ def read_prices(filename):
             prices[row[0]] = float(row[1])
 
     return prices
+
+
+# read the portfolio and the prices
+portfolio = read_portfolio('Data/portfolio.csv')
+prices = read_prices('Data/prices.csv')
+
+total_cost = 0.0    # total cost of buying all stock shares
+total_value = 0.0   # current value of all stock shares
+
+for stock in portfolio: # loop over stocks in portfolio
+    # add this stock shares cost and value
+    total_cost += stock['price'] * stock['shares']
+    total_value += prices[stock['name']] * stock['shares']
+
+print(f'Current Stocks Value: {total_value:0.2f}')
+print(f'Net Profit (Gain/Loss): {total_value-total_cost:0.2f}')
