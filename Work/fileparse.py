8b77a50e163479c3b799594f97e2ff9824f0a051
mohammed asfour <masfour@mun.ca>
2021-05-29 12:50:17 +0200

    solution to exercise 3.5

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,10 +1,10 @@
 # fileparse.py
 #
-# Exercise 3.4
+# Exercise 3.5
 
 import csv
 
-def parse_csv(filename, select=None):
+def parse_csv(filename, select=None, types=None):
     """Parse a CSV file into a list of records with safe selection of columns
 
     Args:
@@ -34,10 +34,15 @@ def parse_csv(filename, select=None):
         else:
             indices = range(len(headers))   # all values in the row if no headers were selected
 
+        # default the types to str if not passeed
+        if not types:
+            types = [str for h in headers]
+
         for row in rows:
             if not row:    # Skip rows with no data
                 continue
-            record = dict(zip(headers, [row[col_idx] for col_idx in indices]))
+            # cast the selected column data into the corresponding type
+            record = dict(zip(headers, [val_type(row[col_idx]) for val_type, col_idx in zip(types, indices)]))
             records.append(record)
 
     return records
